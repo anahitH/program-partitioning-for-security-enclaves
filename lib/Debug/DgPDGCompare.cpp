a9f56a113d743a6578b79d3f9b9249a4623ff7fb
anahitH <anahithayrapetyan91@gmail.com>
2018-10-17 11:57:15 +0200

    Implemented creation of boost graph from PDG

--- lib/Debug/DgPDGCompare.cpp
+++ lib/Debug/DgPDGCompare.cpp
@@ -38,8 +38,14 @@ directed_graph create_graph_from_dg(dg::LLVMDependenceGraph* dg) noexcept
     directed_graph graph{};
     std::unordered_map<llvm::Value*, vertex_desc> nodes_mapping; 
     for (auto it = dg->begin(); it != dg->end(); ++it) {
-        auto vd = boost::add_vertex(graph);
-        nodes_mapping.insert(std::make_pair(it->first, vd));
+        vertex_desc vd;
+        auto pos = nodes_mapping.find(it->first);
+        if (pos == nodes_mapping.end()) {
+            vd = boost::add_vertex(graph);
+            nodes_mapping.insert(std::make_pair(it->first, vd)).first->second;
+        } else {
+            vd = pos->second;
+        }
         for (auto edge_it = it->second->data_begin();
                 edge_it != it->second->data_end();
                 ++edge_it) {
@@ -52,6 +58,50 @@ directed_graph create_graph_from_dg(dg::LLVMDependenceGraph* dg) noexcept
                 dest_vd = dest_node->second;
             }
             boost::add_edge(vd, dest_vd, graph);
+        }
+    }
+    return graph;
+}
+
+directed_graph create_graph_from_pdg(llvm::Function& F, SVFG* svfg)
+{
+    directed_graph graph{};
+    auto pag = svfg->getPAG();
+    std::unordered_map<llvm::Value*, vertex_desc> nodes_mapping;
+    for (auto& B : F) {
+        for (auto& I : B) {
+            if (!pag->hasValueNode(&I)) {
+                continue;
+            }
+            vertex_desc vd;
+            auto pos = nodes_mapping.find(&I);
+            if (pos == nodes_mapping.end()) {
+                vd = boost::add_vertex(graph);
+                nodes_mapping.insert(std::make_pair(&I, vd)).first->second;
+            } else {
+                vd = pos->second;
+            }
+            auto nodeID = pag->getValueNode(&I);
+            auto node = pag->getPAGNode(nodeID);
+            if (!node->hasOutgoingEdge()) {
+                continue;
+            }
+            const auto& edges = node->getOutEdges();
+            for (auto edge : edges) {
+                auto destNode = edge->getDstNode();
+                if (!destNode->getValue()) {
+                    continue;
+                }
+                vertex_desc dest_vd;
+                auto dest_node = nodes_mapping.find(const_cast<llvm::Value*>(destNode->getValue()));
+                if (dest_node == nodes_mapping.end()) {
+                    dest_vd = boost::add_vertex(graph);
+                    nodes_mapping.insert(std::make_pair(const_cast<llvm::Value*>(destNode->getValue()), dest_vd));
+                } else {
+                    dest_vd = dest_node->second;
+                }
+                boost::add_edge(vd, dest_vd, graph);
+            }
         }
     }
     return graph;
@@ -95,8 +145,12 @@ public:
                 continue;
             }
             auto dg_graph = create_graph_from_dg(F_dg->second);
-            llvm::dbgs() << "Vertices number " << boost::num_vertices(dg_graph) << "\n";
-            llvm::dbgs() << "Edge number " << boost::num_edges(dg_graph) << "\n";
+            llvm::dbgs() << "DG Vertices number " << boost::num_vertices(dg_graph) << "\n";
+            llvm::dbgs() << "DG Edge number " << boost::num_edges(dg_graph) << "\n";
+            auto pdg_graph = create_graph_from_pdg(F, svfg);
+            llvm::dbgs() << "PDG Vertices number " << boost::num_vertices(pdg_graph) << "\n";
+            llvm::dbgs() << "PDG Edge number " << boost::num_edges(pdg_graph) << "\n";
+
         }
                 return false;
     }
