d8c0136e24ac726ebb2d6228572333980a0f758b
anahitH <anahithayrapetyan91@gmail.com>
2019-07-19 12:06:24 +0000

    Added generation of enclave file including wrappers for ocalls

--- lib/CodeGen/OpenEnclaveCodeGenerator.cpp
+++ lib/CodeGen/OpenEnclaveCodeGenerator.cpp
@@ -6,6 +6,21 @@
 namespace vazgen {
 
 namespace {
+
+std::string getEdlParamQualifier(const Type paramty)
+{
+    if (!paramty.m_isPtr && !paramty.m_isArray) {
+	    return "";
+    }
+    std::string qualifier = OpenEnclaveCodeGenerator::IN_PARAM;
+    if (paramty.m_isPtr || paramty.m_isArray) {
+	    if (paramty.m_qualifier != "const") {
+		    qualifier = OpenEnclaveCodeGenerator::IN_OUT_PARAM;
+	    }
+    }
+    return qualifier;
+}
+
 Function generateFunctionForEdl(const Function& function, bool is_protected)
 {
     std::string name = function.getName();
@@ -19,12 +34,7 @@ Function generateFunctionForEdl(const Function& function, bool is_protected)
     // TODO: think about having enum for access modifiers
     enclaveF.setAccessModifier("public");
     for (const auto& param : function.getParams()) {
-        std::string qualifier = OpenEnclaveCodeGenerator::IN_PARAM;
-        if (param.m_type.m_isPtr || param.m_type.m_isArray) {
-            if (param.m_type.m_qualifier != "const") {
-                qualifier = OpenEnclaveCodeGenerator::IN_OUT_PARAM;
-            }
-        }
+	std::string qualifier = getEdlParamQualifier(param.m_type);
         Type enclaveParamTy{param.m_type.m_name, qualifier, param.m_type.m_isPtr, param.m_type.m_isArray};
         enclaveF.addParam({enclaveParamTy, param.m_name});
     }
@@ -41,6 +51,26 @@ Function generateMain()
     main.addBody("app_main()");
     main.addBody("return 0");
     return main;
+}
+
+std::string generateOcall(const Function& appF, const Variable returnVal)
+{
+    std::stringstream ocallStr;
+    ocallStr << "oe_result_t oe_result = " << appF.getName() << "(";
+    if (!appF.isVoidReturn()) {
+        if (!appF.getReturnType().m_isPtr) {
+            ocallStr << "&";
+        }
+        ocallStr << returnVal.m_name << ", ";
+    }
+    for (const auto& param : appF.getParams()) {
+        ocallStr << param.m_name;
+        if (param.m_name != appF.getParams().back().m_name) {
+            ocallStr << ", ";
+        }
+    }
+    ocallStr << ");";
+    return ocallStr.str();
 }
 
 std::string generateEcall(const Function& ecallF, const Variable returnVal)
@@ -91,6 +121,35 @@ Function generateEcallWrapper(const Function& ecallF)
     return wrapperF;
 }
 
+Function generateOcallWrapper(const Function& appF)
+{
+    Function wrapperF(appF.getName());
+    wrapperF.setReturnType(appF.getReturnType());
+    wrapperF.setParams(appF.getParams());
+
+    std::stringstream wrapperBody;
+    Variable returnVal;
+    if (!appF.isVoidReturn()) {
+        returnVal.m_type = appF.getReturnType();
+        returnVal.m_name = "return_val";
+        wrapperBody << returnVal.getAsString() << ";\n";
+    }
+    // TODO: handle pointer type for return value
+    wrapperBody << generateOcall(appF, returnVal) << "\n";
+    wrapperBody << "if (oe_result != OE_OK) {\n";
+    wrapperBody << "fprintf(stderr, \"Failed to invoke ocall " << appF.getName() << "\");\n";
+    // TODO: see how to handle failure of ocalls Terminate enclave??
+    //wrapperBody << "terminate_enclave();\n";
+    //wrapperBody << "exit(1);\n";
+    wrapperBody << "}\n";
+    if (!appF.isVoidReturn()) {
+        wrapperBody << "return " << returnVal.m_name;
+    }
+    wrapperF.addBody(wrapperBody.str());
+    return wrapperF;
+}
+
+
 } // unnamed namespace
 
 const std::string OpenEnclaveCodeGenerator::IN_PARAM = "[in]";
@@ -123,8 +182,8 @@ void OpenEnclaveCodeGenerator::writeGeneratedFiles()
     SourceFileWriter edlFileWriter(m_edlFile);
     edlFileWriter.write();
 
-    //SourceFileWriter enclaveFileWriter(m_enclaveFile);
-    //enclaveFileWriter.write();
+    SourceFileWriter enclaveFileWriter(m_enclaveFile);
+    enclaveFileWriter.write();
 
     SourceFileWriter appDriverFileWriter(m_appDriverFile);
     appDriverFileWriter.write();
@@ -156,6 +215,11 @@ void OpenEnclaveCodeGenerator::generateEnclaveFile()
     m_enclaveFile.setHeader(false);
     m_enclaveFile.addInclude("<openenclave/enclave.h>");
     m_enclaveFile.addInclude("\"sgx_t.h\"");
+
+    for (const auto& appF : m_appFunctions) {
+        m_enclaveFile.addFunction(generateOcallWrapper(appF));
+    }
+
 }
 
 void OpenEnclaveCodeGenerator::generateAppDriverFile()
@@ -166,13 +230,13 @@ void OpenEnclaveCodeGenerator::generateAppDriverFile()
     // app_util.h has enclave creation and termination code
     m_appDriverFile.addInclude("\"app_utils.h\"");
 
+    // TODO: add extern
     m_enclaveFile.addGlobalVariable(std::make_pair(Variable{Type{"oe_enclave_t", "", true, false}, "enclave"}, "NULL"));
 
     Function appMainF("app_main");
     appMainF.setIsExtern(true);
     appMainF.setReturnType(Type{"int", "", false, false});
     m_appDriverFile.addFunction(appMainF);
-
 
     m_appDriverFile.addFunction(generateMain());
     for (const auto& secureF : m_enclaveFunctions) {
