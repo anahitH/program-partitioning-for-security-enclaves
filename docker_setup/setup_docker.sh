e6424ac4d716ca6b01d23aef345f1328fc8a985d
anahitH <anahithayrapetyan91@gmail.com>
2019-08-26 19:16:02 +0200

    Changed setup_docker.sh script to pull all necessary projects locally and create a zip file

--- docker_setup/setup_docker.sh
+++ docker_setup/setup_docker.sh
@@ -7,6 +7,11 @@ git clone https://github.com/google/asylo.git
 cp -r asylo/asylo/distrib/toolchain/ ./
 cp Dockerfile ./toolchain
 cp COSCE129LIN64.bin ./toolchain
+cp -r SVF ./toolchain
+cp -r program-dependence-graph ./toolchain
+cp -r program-partitioning-for-security-enclaves ./toolchain
+cp -r spdlog ./toolchain
+cp -r json ./toolchain
 cp installer.properties ./toolchain
 
 cd ./toolchain
